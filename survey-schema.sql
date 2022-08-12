43eeb0cdeffec758d973af1e2e6ddffd1d7204a1
Austin Chao <austinschao@gmail.com>
2022-08-12 15:19:22 -0700

    Added sql schema

---
+++ survey-schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE survey (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    experience INT NOT NULL,
+    education INT NOT NULL,
+    frontend INT NOT NULL,
+    backend INT NOT NULL,
+    language INT NOT NULL,
+    sent_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+)
--- survey.sql
+++ survey.sql
@@ -5,17 +5,7 @@ DROP DATABASE survey;
 CREATE DATABASE survey;
 \connect survey
 
-CREATE TABLE survey (
-    id SERIAL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    experience INT NOT NULL,
-    education INT NOT NULL,
-    frontend INT NOT NULL,
-    backend INT NOT NULL,
-    language INT NOT NULL,
-    sent_at TIMESTAMP WITH TIME ZONE
-)
+\i survey-schema.sql
 
 \echo 'Delete and recreate survey_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
@@ -24,14 +14,4 @@ DROP DATABASE survey_test;
 CREATE DATABASE survey_test;
 \connect survey_test
 
-CREATE TABLE survey (
-    id SERIAL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    experience INT NOT NULL,
-    education INT NOT NULL,
-    frontend INT NOT NULL,
-    backend INT NOT NULL,
-    language INT NOT NULL,
-    sent_at TIMESTAMP WITH TIME ZONE
-)
+\i survey-schema.sql
